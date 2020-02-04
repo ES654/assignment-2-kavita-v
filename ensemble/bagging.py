d68e046388378b57428c41105b948d57bdfb34fc
kavita-v <54227800+kavita-v@users.noreply.github.com>
2020-02-04 18:10:20 +0530

    Update bagging.py

--- ensemble/bagging.py
+++ ensemble/bagging.py
@@ -1,4 +1,13 @@
 
+import numpy as np 
+import pandas as pd
+from random import randrange
+from statistics import mode
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn import tree
+import copy
+
 class BaggingClassifier():
     def __init__(self, base_estimator, n_estimators=100):
         '''
@@ -6,7 +15,8 @@ class BaggingClassifier():
                                You can pass the object of the estimator class
         :param n_estimators: The number of estimators/models in ensemble.
         '''
-
+        self.base_estimator = base_estimator
+        self.n_estimators = n_estimators
         pass
 
     def fit(self, X, y):
@@ -16,7 +26,31 @@ class BaggingClassifier():
         X: pd.DataFrame with rows as samples and columns as features (shape of X is N X P) where N is the number of samples and P is the number of columns.
         y: pd.Series with rows corresponding to output variable (shape of Y is N)
         """
-        pass
+        models = []
+        bags_X = []
+        bags_y = []
+        for i in range(self.n_estimators):
+            bag_X = []
+            bag_y = []
+            X_copy = X.values
+            y_copy = y.values
+            while len(bag_X) < len(X):
+                index = randrange(len(X_copy))
+                bag_X.append(X_copy[index])
+                bag_y.append(y_copy[index])
+            model = self.base_estimator
+            model.fit(bag_X,bag_y)
+            # print(tree.export_graphviz(model))
+            models.append(copy.deepcopy(model))
+            bags_X.append(bag_X)
+            bags_y.append(bag_y)
+
+        self.models = models
+        self.X = X
+        self.y = y
+        self.bags_X = bags_X
+        self.bags_y = bags_y
+        return self
 
     def predict(self, X):
         """
@@ -26,7 +60,15 @@ class BaggingClassifier():
         Output:
         y: pd.Series with rows corresponding to output variable. THe output variable in a row is the prediction for sample in corresponding row in X.
         """
-        pass
+        y_pred = []
+        for i in range(len(X)):
+            preds=[]
+            X_test = X[X.index==i]
+            for model in self.models:
+                pred = model.predict(X_test)
+                preds.append(pred[0])
+            y_pred.append(mode(preds))
+        return pd.Series(y_pred)
 
     def plot(self):
         """
@@ -42,4 +84,53 @@ class BaggingClassifier():
         This function should return [fig1, fig2]
 
         """
-        pass
+        h = .02
+        i=1
+        bags_X = self.bags_X
+        bags_y = self.bags_y
+        fig1 = plt.figure(figsize=(45, 9))
+
+    
+        cm = plt.cm.RdBu
+        cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+       
+        for model in self.models:
+            ax = plt.subplot(1, len(self.models) , i)
+            X = pd.DataFrame(bags_X[i-1])
+            y = pd.Series(bags_y[i-1])
+            x_min, x_max = X[X.columns[0]].min() - .5, X[X.columns[0]].max() + .5
+            y_min, y_max = X[X.columns[1]].min() - .5, X[X.columns[1]].max() + .5
+            xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+            Z = np.array(model.predict(pd.DataFrame(np.c_[xx.ravel(), yy.ravel()], columns=X.columns)))
+            # print(Z[12])
+            Z = Z.reshape(xx.shape)
+            ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+            ax.scatter(X[X.columns[0]], X[X.columns[1]], c=y, cmap=cm_bright, edgecolors='k')
+            # size=[1000*w for w in self.weights[i-1]]
+            ax.set_xlim(xx.min(), xx.max())
+            ax.set_ylim(yy.min(), yy.max())
+            ax.set_xlabel(str(X.columns[0]))
+            ax.set_ylabel(str(X.columns[1]))
+            plt.title("Estimator "+str(i))
+            i+=1
+            
+        fig2 = plt.figure(figsize=(9,9))
+        X = self.X
+        y = self.y
+        ax2 = plt.subplot(1,1,1)
+        x_min, x_max = X[X.columns[0]].min() - .5, X[X.columns[0]].max() + .5
+        y_min, y_max = X[X.columns[1]].min() - .5, X[X.columns[1]].max() + .5
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+        Z = np.array(self.predict(pd.DataFrame(np.c_[xx.ravel(), yy.ravel()], columns=X.columns)))
+        Z = Z.reshape(xx.shape)
+        ax2.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+        # size=[1000*w for w in self.weights[i-2]]
+        ax2.scatter(X[X.columns[0]], X[X.columns[1]], c=y, cmap=cm_bright, edgecolors='k')
+        ax2.set_xlim(xx.min(), xx.max())
+        ax2.set_ylim(yy.min(), yy.max())
+        plt.title("Combined Decision Surface")
+        
+        plt.tight_layout()
+        plt.show()
+
+        return [fig1,fig2]
