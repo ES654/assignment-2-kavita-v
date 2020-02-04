c5fd5df19172b9eca186d0fb2b6b77040c168412
kavita-v <54227800+kavita-v@users.noreply.github.com>
2020-02-04 18:25:15 +0530

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pandas as pd
+
 
 def accuracy(y_hat, y):
     """
@@ -15,8 +18,17 @@ def accuracy(y_hat, y):
     ensure that the function does not fail in corner cases.
     """
     assert(y_hat.size == y.size)
-    # TODO: Write here
-    pass
+    y_hat = list(y_hat)
+    y = list(y)
+    total = len(y)
+    accurate = 0.0
+    for i in range(total):
+        if y_hat[i] == y[i]:
+            accurate += 1
+    accuracy = accurate/total
+
+    return accuracy
+
 
 def precision(y_hat, y, cls):
     """
@@ -29,7 +41,22 @@ def precision(y_hat, y, cls):
     Output:
     > Returns the precision as float
     """
-    pass
+    assert(y_hat.size == y.size)
+    y_hat = list(y_hat)
+    y = list(y)
+    total = 0.0
+    precise = 0.0
+    for i in range(len(y)):
+        if y_hat[i] == cls:
+            if y[i] == cls:
+                precise += 1
+            total += 1
+    if total==0:
+        return 0.0
+    precision = precise/total
+    
+    return precision
+
 
 def recall(y_hat, y, cls):
     """
@@ -42,7 +69,19 @@ def recall(y_hat, y, cls):
     Output:
     > Returns the recall as float
     """
-    pass
+    assert(y_hat.size == y.size)
+    y_hat = list(y_hat)
+    y = list(y)
+    total = 0.0
+    marked = 0.0
+    for i in range(len(y)):
+        if y[i] == cls:
+            if y_hat[i] == cls:
+                marked += 1
+            total += 1
+    recall = marked/total
+
+    return recall
 
 def rmse(y_hat, y):
     """
@@ -55,7 +94,15 @@ def rmse(y_hat, y):
     > Returns the rmse as float
     """
 
-    pass
+    assert(y_hat.size == y.size)
+    y_hat = list(y_hat)
+    y = list(y)
+    sum = 0.0
+    for i in range(len(y)):
+        sum += (y_hat[i] - y[i]) ** 2
+    rmse = (sum/len(y))**(1/2)
+
+    return rmse
 
 def mae(y_hat, y):
     """
@@ -67,4 +114,12 @@ def mae(y_hat, y):
     Output:
     > Returns the mae as float
     """
-    pass
+    assert(y_hat.size == y.size)
+    y_hat = list(y_hat)
+    y = list(y)
+    abs_sum = 0.0
+    for i in range(len(y)):
+        abs_sum += abs(y_hat[i] - y[i])
+    mae = abs_sum / len(y)
+
+    return mae
